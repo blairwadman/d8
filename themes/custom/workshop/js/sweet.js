e07c53fb93c46e01ca3a15b3146710243b9e297b
Blair Wadman <blair@wadmanconsulting.com>
2018-06-06 09:18:44 +0100

    Added Sweet Alert 3rd party JS library

---
+++ themes/custom/workshop/js/sweet.js
@@ -0,0 +1,9 @@
+(function ($, Drupal) {
+    'use strict';
+
+    Drupal.behaviors.sweet = {
+        attach: function attach(context, settings) {
+            //swal("Hello world!");
+        }
+    };
+})(jQuery, Drupal);
